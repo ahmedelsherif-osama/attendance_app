01acf60981742b8c3dfb3910db4624a5ce60c2d5
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-02-11 18:40:36 +0400

    Updated controllers for BusRouteScreen2 to display data from state
    Updated controllers for BusRouteScreen2 to display data from state

--- lib/presentation/screens_widgets/3_bus_route_screen2.dart
+++ lib/presentation/screens_widgets/3_bus_route_screen2.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_rta_attendance/cubit/app_cubit.dart';
+import 'package:final_rta_attendance/models/1_school_model.dart';
 import 'package:final_rta_attendance/models/2_bus_route_model.dart';
 import 'package:final_rta_attendance/presentation/screens_widgets/1_school_list.dart';
 import 'package:final_rta_attendance/presentation/screens_widgets/4_attendance_record_screen.dart';
@@ -14,7 +16,13 @@ class BusRouteScreen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    BusRouteModel busRoute = context.read<AppCubit>().state.currentBusRoute;
+    final BusRouteModel busRoute =
+        context.read<AppCubit>().state.currentBusRoute;
+    final SchoolModel school = context.read<AppCubit>().state.currentSchool;
+    final busRouteNumberController =
+        TextEditingController(text: busRoute.busRouteNumber.toString());
+    final schoolNameController =
+        TextEditingController(text: school.name.toString());
 
     // List studentList = busRoute.studentsIDs;
     List studentList = [
@@ -30,110 +38,123 @@ class BusRouteScreen2 extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.symmetric(
             vertical: height * 0.1, horizontal: width * 0.1),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: width * 0.2,
-                  child: const Text("Bus Route: "),
-                ),
-                SizedBox(
-                  width: width * 0.1,
-                  child: TextFormField(initialValue: "1"),
-                ),
-                SizedBox(
-                  width: width * 0.2,
-                  child: const Text("School: "),
-                ),
-                SizedBox(
-                  width: width * 0.3,
-                  child: TextFormField(initialValue: "Apple School"),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: height * 0.1,
-            ),
-            SizedBox(
-              height: height * 0.05,
-              child: const Text("Students:"),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: studentList.length,
-                itemBuilder: (context, index) {
-                  print(studentList[index]);
-                  return Text(studentList[index].toString());
-                },
-              ),
-            ),
-            SizedBox(height: height * 0.1),
-            Column(
-              children: [
-                CustomButton(
-                  height: height,
-                  width: width,
-                  text: "Take Attendance",
-                  color: Colors.green,
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => AttendanceRecordScreen(),
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: height * 0.015,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: StreamBuilder<Object>(
+            stream:
+                FirebaseFirestore.instance.collection('students').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
                   children: [
-                    CustomButton(
-                      height: height,
-                      width: width,
-                      color: Colors.blue,
-                      text: "Save Changes",
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(route);
-                      },
-                    ),
-                    CustomButton(
-                      height: height,
-                      width: width,
-                      color: Colors.blue,
-                      text: "Add Student",
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => AddStudentScreen(),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: width * 0.2,
+                          child: const Text("Bus Route: "),
+                        ),
+                        SizedBox(
+                          width: width * 0.1,
+                          child: TextFormField(
+                            controller: busRouteNumberController,
                           ),
-                        );
-                      },
+                        ),
+                        SizedBox(
+                          width: width * 0.2,
+                          child: const Text("School: "),
+                        ),
+                        SizedBox(
+                          width: width * 0.3,
+                          child: TextFormField(
+                            controller: schoolNameController,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: height * 0.1,
+                    ),
+                    SizedBox(
+                      height: height * 0.05,
+                      child: const Text("Students:"),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: studentList.length,
+                        itemBuilder: (context, index) {
+                          print(studentList[index]);
+                          return Text(studentList[index].toString());
+                        },
+                      ),
+                    ),
+                    SizedBox(height: height * 0.1),
+                    Column(
+                      children: [
+                        CustomButton(
+                          height: height,
+                          width: width,
+                          text: "Take Attendance",
+                          color: Colors.green,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => AttendanceRecordScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: height * 0.015,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            CustomButton(
+                              height: height,
+                              width: width,
+                              color: Colors.blue,
+                              text: "Save Changes",
+                              onTap: () {
+                                Navigator.of(context).pushReplacement(route);
+                              },
+                            ),
+                            CustomButton(
+                              height: height,
+                              width: width,
+                              color: Colors.blue,
+                              text: "Add Student",
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => AddStudentScreen(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: height * 0.015,
+                        ),
+                        CustomButton(
+                          height: height,
+                          width: width,
+                          color: Colors.red,
+                          text: "Delete Bus Route",
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => SchoolListScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ],
-                ),
-                SizedBox(
-                  height: height * 0.015,
-                ),
-                CustomButton(
-                  height: height,
-                  width: width,
-                  color: Colors.red,
-                  text: "Delete Bus Route",
-                  onTap: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => SchoolListScreen(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
+                );
+              } else {
+                return Center(child: const CircularProgressIndicator());
+              }
+            }),
       ),
     );
   }
