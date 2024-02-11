d0af25241805df54f5caa3b98d1d3c282b53b2c7
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-02-11 14:38:09 +0400

    Added onTap function to CustomButton widget.
    Added onTap function to CustomButton widget.

--- lib/presentation/widgets/custom_button.dart
+++ lib/presentation/widgets/custom_button.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton(
-      {required this.height,
-      required this.width,
-      required this.text,
-      required this.color});
+  const CustomButton({
+    required this.height,
+    required this.width,
+    required this.text,
+    required this.color,
+    required this.onTap,
+  });
   final height;
   final width;
   final String text;
   final Color color;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height * 0.05,
-      width: width * 0.3,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(
-            color: Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: height * 0.05,
+        width: width * 0.3,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
