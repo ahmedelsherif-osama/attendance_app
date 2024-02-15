774687cce3c1a7941acaa8120a3ed8014e26718b
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-02-15 09:41:26 +0400

    removed extra/redundant confusing save icons, already have a save textbutton
    removed extra/redundant confusing save icons, already have a save textbutton

--- lib/presentation/widgets/change_details_popup.dart
+++ lib/presentation/widgets/change_details_popup.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChangeDetailsPopup extends StatefulWidget {
@@ -12,6 +13,27 @@ class _ChangeDetailsPopupState extends State<ChangeDetailsPopup> {
   bool _isEditingSchool = false;
   bool _isEditingBusRouteNumber = false;
   bool _isAddingNewSchool = false;
+  List<String>? schoolNames;
+  void initState() {
+    super.initState();
+    _fetchSchools().then((names) {
+      setState(() {
+        schoolNames = names;
+      });
+    });
+  }
+
+  Future<List<String>> _fetchSchools() async {
+    try {
+      var schools =
+          await FirebaseFirestore.instance.collection('schools').get();
+      return schools.docs.map((doc) => doc['name'] as String).toList();
+    } catch (e) {
+      // Handle any errors that occurred during the fetch
+      print('Error fetching schools: $e');
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,15 +104,15 @@ class _ChangeDetailsPopupState extends State<ChangeDetailsPopup> {
                     )
                   : _buildDropdownMenu(),
             ),
-            IconButton(
-              icon: Icon(_isEditingSchool ? Icons.save : Icons.edit),
-              onPressed: () {
-                setState(() {
-                  _isEditingSchool = !_isEditingSchool;
-                  _isAddingNewSchool = false;
-                });
-              },
-            ),
+            // IconButton(
+            //   icon: Icon(_isEditingSchool ? Icons.save : Icons.edit),
+            //   onPressed: () {
+            //     setState(() {
+            //       _isEditingSchool = !_isEditingSchool;
+            //       _isAddingNewSchool = false;
+            //     });
+            //   },
+            // ),
           ],
         ),
         SizedBox(height: 8.0),
@@ -114,10 +136,9 @@ class _ChangeDetailsPopupState extends State<ChangeDetailsPopup> {
 
   Widget _buildDropdownMenu() {
     // Replace this with the actual list of schools from your data source
-    List<String> schoolNames = ['School A', 'School B', 'School C'];
 
     return DropdownButtonFormField<String>(
-      items: schoolNames.map((String school) {
+      items: schoolNames!.map((String school) {
         return DropdownMenuItem<String>(
           value: school,
           child: Text(school),
@@ -154,10 +175,10 @@ class _ChangeDetailsPopupState extends State<ChangeDetailsPopup> {
                   ),
                 ),
         ),
-        IconButton(
-          icon: Icon(isEditing ? Icons.save : Icons.edit),
-          onPressed: onPressed,
-        ),
+        // IconButton(
+        //   icon: Icon(isEditing ? Icons.save : Icons.edit),
+        //   onPressed: onPressed,
+        // ),
       ],
     );
   }
